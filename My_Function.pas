26595b06e5bc19032f788d19c1bbbfe1227af5ad
TwistedNoseHair1 <39237924+TwistedNoseHair1@users.noreply.github.com>
2018-05-13 22:40:42 +1000

    user 2 area change

--- My_Function.pas
+++ My_Function.pas
@@ -7,7 +7,11 @@ Var
 	roundedValue: Integer;
 begin
 	cirRadius := ReadInteger('enter Radius: ');
-	cirArea := Pi() * Sqr(CirRadius);
+	if cirArea = 0 then
+	begin
+		WriteLn('area can't be 0');
+	end.
+	cirArea := Pi() * Sqr(CirRadius) * 10;
 	WriteLn('Circles area is ', cirArea:4:2);
 	roundedValue :=Round(cirArea);
 	WriteLn('Which rounded off has a value of ', roundedValue);
