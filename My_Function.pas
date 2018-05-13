f1b337df2760769b9b7b8b251e7ac971b23f6968
Duncan Mackay <37164192+TwistedNoseHair@users.noreply.github.com>
2018-05-13 22:03:12 +1000

    Fixed original code

--- My_Function.pas
+++ My_Function.pas
@@ -7,7 +7,7 @@ Var
 	roundedValue: Integer;
 begin
 	cirRadius := ReadInteger('enter Radius: ');
-	cirArea := Pi() * Sqr(CirRadius) * 10;
+	cirArea := Pi() * Sqr(CirRadius);
 	WriteLn('Circles area is ', cirArea:4:2);
 	roundedValue :=Round(cirArea);
 	WriteLn('Which rounded off has a value of ', roundedValue);
