ae84aeb53464ab07134c4c2d59c89213b7ad19da
TwistedNoseHair1 <39237924+TwistedNoseHair1@users.noreply.github.com>
2018-05-13 22:41:18 +1000

    Merge conflict fix

--- My_Function.pas
+++ My_Function.pas
@@ -7,13 +7,10 @@ Var
 	roundedValue: Integer;
 begin
 	cirRadius := ReadInteger('enter Radius: ');
-<<<<<<< HEAD
 	if cirArea = 0 then
 	begin
 		WriteLn('area can't be 0');
 	end.
-=======
->>>>>>> 91a9dae3daa0c8ecea5daf78038145ed98c385bb
 	cirArea := Pi() * Sqr(CirRadius) * 10;
 	WriteLn('Circles area is ', cirArea:4:2);
 	roundedValue :=Round(cirArea);
